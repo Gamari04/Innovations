cdcb100be29bbf26e8cb1b81aecff9c8c8fc43cc
Salma <salmaelgmiri04@gmail.com>
2023-10-26 15:05:53 +0200

    L'ajout du dropdown de la partie FAQ (JS)

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-const menuHamburger = document.querySelector(".menu_burger")
-const navLinks = document.querySelector(".links")
 
-menuHamburger.addEventListener('click',()=>{
-navLinks.classList.toggle("mobile-menu")
-})
+const menuHamburger = document.querySelector(".menu_burger");
+const navLinks = document.querySelector(".links");
+
+menuHamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("mobile-menu");
+});
+
+// var questions = document.querySelectorAll(".question");
+// questions.forEach(function(question) {
+//     question.addEventListener("click", function() {
+//         // Sélectionner la réponse associée à la question
+//         var answer = question.querySelector(".answer");
+//         answer.classList.toggle("active");
+//     });
+// });
+const questions = document.querySelectorAll(".ask");
+questions.forEach((question) => {
+  question.addEventListener("click", function () {
+     var answer = question.querySelector(".answer");
+    answer.classList.toggle("open");
+  });
+});
+
